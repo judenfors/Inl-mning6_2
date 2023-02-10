13febd74741453701775b06aed50fe593466e3dc
melvinsjostrand <111962656+Dungeater1337@users.noreply.github.com>
2023-02-10 11:10:29 +0100

    Add project files.

---
+++ Inlämning6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inlämning6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in ett till tal");
+            int tal2 = int.Parse(Console.ReadLine());
+
+            int störst
+
+            Console.WriteLine(Störst);
+        }
+    }
+}
